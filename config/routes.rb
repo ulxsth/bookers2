0b6b6c5e74207d946186baa302df46ab6ea60ac5
ulxsth <ulxsth@gmail.com>
2023-03-18 12:46:55 +0000

    [UPDATE] ルーティングを整理、追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/index'
+  root to: 'homes#index'
+  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:index, :show, :edit]
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
