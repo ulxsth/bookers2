838bb3c5294a5b1fbf684da55cfa26818ada0475
ulxsth <ulxsth@gmail.com>
2023-03-19 22:49:12 +0000

    [FIX] プロフィール画像が表示されない不具合の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,12 @@ class User < ApplicationRecord
   has_many :books
   has_one_attached :image
   
-  def get_image
+  def get_image(width, height)
     unless image.attached?
-      path = Rails.root.join('app/assets/images/no_image.jpg')
+      path = Rails.root.join('app/assets/images/no-image.jpg')
       image.attach(io: File.open(path), filename: 'default-image.png', content_type: 'image/jpeg')
     end
+    image.variant(resize_to_limit: [width, height]).processed
+    image
   end
 end
