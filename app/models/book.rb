f4cea18d34eb4df2c1192c9788584a4a0d9b50d5
ulxsth <ulxsth@gmail.com>
2023-03-20 00:37:23 +0000

    [UPDATE] book一覧を表示

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,13 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  
+  def get_image(width, height)
+    unless image.attached?
+      path = Rails.root.join('app/assets/images/no-image.jpg')
+      image.attach(io: File.open(path), filename: 'default-image.png', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+    image
+  end
 end
