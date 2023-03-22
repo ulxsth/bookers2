dbda92c1f083118a3ae40f8138179aacee82f86f
ulxsth <ulxsth@gmail.com>
2023-03-22 21:37:11 +0000

    [ADD] book#create,edit時にフラッシュメッセージを表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = 'You have created book successfully.'
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -26,6 +27,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = 'You have updated book successfully.'
       redirect_to book_path(@book.id)
     else
       render :edit
