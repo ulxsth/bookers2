b57332db8f0c57c4ff1f01b5866dcd5aec2423b1
ulxsth <ulxsth@gmail.com>
2023-03-27 03:43:13 +0000

    [FINISH] RSpecによるテスト完了

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
+    favorite.save!
     redirect_back(fallback_location: books_path)
   end
   
